393ed643f8bf082f6e149518c9f9060679ac5542
orange2120 <orange21201@gmail.com>
2020-12-06 19:03:48 +0800

    HW4 update

--- HW4/HW3.v
+++ HW4/HW3.v
@@ -22,36 +22,63 @@ module HW3(clk,
     wire [6:0] func7 = mem_rdata_I[31:25];
 
     parameter NONE_TYPE = 23'bx;
+    parameter JAL  = {1'b1, 22'b0};
+    parameter JALR = {1'b0, 1'b1, 21'b0};
+    parameter BEQ  = {2'b0, 1'b1, 20'b0};
+    parameter BNE  = {3'b0, 1'b1, 19'b0};
+    parameter ADDI = {6'b0, 1'b1, 16'b0};
+    parameter SLTI = {7'b0, 1'b1, 15'b0};
+    parameter XORI = {8'b0, 1'b1, 14'b0};
+    parameter ORI  = {9'b0, 1'b1, 13'b0};
+    parameter ANDI = {10'b0, 1'b1, 12'b0};
+    parameter SLLI = {11'b0, 1'b1, 11'b0};
+    parameter SRLI = {12'b0, 1'b1, 10'b0};
+    parameter SRAI = {13'b0, 1'b1, 9'b0};
+    parameter ADD  = {14'b0, 1'b1, 8'b0};
+    parameter SUB  = {15'b0, 1'b1, 7'b0};
+    parameter SLL  = {16'b0, 1'b1, 6'b0};
+    parameter SLT  = {17'b0, 1'b1, 5'b0};
+    parameter XOR  = {18'b0, 1'b1, 4'b0};
+    parameter SRL  = {19'b0, 1'b1, 3'b0};
+    parameter SRA  = {20'b0, 1'b1, 2'b0};
+    parameter OR   = {21'b0, 1'b1, 1'b0};
+    parameter AND  = {22'b0, 1'b1};
+
     parameter NONE_FORMAT = 5'bx;
+    parameter R_FORMAT = 5'b10000;
+    parameter I_FORMAT = 5'b01000;
+    parameter S_FORMAT = 5'b00100;
+    parameter B_FORMAT = 5'b00010;
+    parameter J_FORMAT = 5'b00001;
 
     always @(*) begin
             mem_addr_reg = mem_addr_I + 1'b1;
 
             case (mem_rdata_I[6:0]) // opcode
                 7'b1101111: begin // UJ type
-                    instruction_format_reg = {5'b00001};
-                    instruction_type_reg = {1'b1, 22'b0}; // JAL
+                    instruction_format_reg = J_FORMAT;
+                    instruction_type_reg = JAL; // JAL
                 end
 
                 7'b1100111: begin // I type
-                    instruction_format_reg = {5'b01000};
-                    instruction_type_reg = {1'b0, 1'b1, 21'b0}; // JALR
+                    instruction_format_reg = I_FORMAT;
+                    instruction_type_reg = JALR;// JALR
                 end
 
                 7'b1100011: begin // SB type
-                    instruction_format_reg = {5'b00010};
+                    instruction_format_reg = B_FORMAT;
                     case (func3)
                         3'b000:
-                            instruction_type_reg = {2'b0, 1'b1, 20'b0}; // BEQ
+                            instruction_type_reg = BEQ; // BEQ
                         3'b001:
-                            instruction_type_reg = {3'b0, 1'b1, 19'b0}; // BNE
+                            instruction_type_reg = BNE; // BNE
                         default:
                             instruction_type_reg = NONE_TYPE;
                     endcase
                 end
 
                 7'b0000011: begin // I type
-                    instruction_format_reg = {5'b01000};
+                    instruction_format_reg = I_FORMAT;
                     case (func3)
                         3'b011:
                             instruction_type_reg = {4'b0, 1'b1, 18'b0}; // LD
@@ -61,7 +88,7 @@ module HW3(clk,
                 end
 
                 7'b0100011: begin // S type
-                    instruction_format_reg = {5'b00100};
+                    instruction_format_reg = S_FORMAT;
                     case (func3)
                         3'b011: 
                             instruction_type_reg = {5'b0, 1'b1, 17'b0}; // SD
@@ -73,19 +100,19 @@ module HW3(clk,
                 7'b0010011: begin // I type
                     case (func3)
                         3'b000:
-                        instruction_type_reg = {6'b0, 1'b1, 16'b0}; // ADDI
+                        instruction_type_reg = ADDI; // ADDI
                         3'b010:
-                        instruction_type_reg = {7'b0, 1'b1, 15'b0}; // SLTI
+                        instruction_type_reg = SLTI; // SLTI
                         3'b100:
-                        instruction_type_reg = {8'b0, 1'b1, 14'b0}; // XORI
+                        instruction_type_reg = XORI; // XORI
                         3'b110:
-                        instruction_type_reg = {9'b0, 1'b1, 13'b0}; // ORI
+                        instruction_type_reg = ORI; // ORI
                         3'b111: 
-                        instruction_type_reg = {10'b0, 1'b1, 12'b0}; // ANDI
+                        instruction_type_reg = ANDI; // ANDI
                         3'b001:
                         case (func7)
                             7'b0: 
-                                instruction_type_reg = {11'b0, 1'b1, 11'b0}; // SLLI
+                                instruction_type_reg = SLLI; // SLLI
                             default:
                                 instruction_type_reg = NONE_TYPE;
                         endcase
@@ -93,9 +120,9 @@ module HW3(clk,
                         3'b101:
                         case (func7)
                             7'b0:
-                                instruction_type_reg = {12'b0, 1'b1, 10'b0}; // SRLI
+                                instruction_type_reg = SRLI; // SRLI
                             7'b0100000:
-                                instruction_type_reg = {13'b0, 1'b1, 9'b0};  // SRAI
+                                instruction_type_reg = SRAI;  // SRAI
                             default:
                                 instruction_type_reg = NONE_TYPE;
                         endcase
@@ -105,7 +132,7 @@ module HW3(clk,
                             instruction_type_reg = {23'bx};
                         end
                     endcase
-                    instruction_format_reg = {5'b01000};
+                    instruction_format_reg = I_FORMAT;
                 end
 
                 7'b0110011: begin // R type
@@ -113,38 +140,38 @@ module HW3(clk,
                         3'b000:
                         case (func7)
                             7'b0:
-                                instruction_type_reg = {14'b0, 1'b1, 8'b0}; // ADD
+                                instruction_type_reg = ADD; // ADD
                             7'b0100000:
-                                instruction_type_reg = {15'b0, 1'b1, 7'b0}; // SUB     
+                                instruction_type_reg = SUB; // SUB     
                             default:
                                 instruction_type_reg = NONE_TYPE;
                         
                         endcase
                         3'b001:
-                        instruction_type_reg = {16'b0, 1'b1, 6'b0}; // SLL
+                        instruction_type_reg = SLL; // SLL
                         3'b010:
-                        instruction_type_reg = {17'b0, 1'b1, 5'b0}; // SLT
+                        instruction_type_reg = SLT; // SLT
                         3'b100:
-                        instruction_type_reg = {18'b0, 1'b1, 4'b0}; // XOR
+                        instruction_type_reg = XOR; // XOR
                         3'b101:
                         case (func7)
                             7'b0:
-                                instruction_type_reg = {19'b0, 1'b1, 3'b0}; // SRL
+                                instruction_type_reg = SRL; // SRL
                             7'b0100000:
-                                instruction_type_reg = {20'b0, 1'b1, 2'b0}; // SRA
+                                instruction_type_reg = SRA; // SRA
                             default:
                                 instruction_type_reg = NONE_TYPE;
                         endcase
                         3'b110:
-                        instruction_type_reg = {21'b0, 1'b1, 1'b0}; // OR 
+                        instruction_type_reg = OR; // OR 
                         3'b111:
-                        instruction_type_reg = {22'b0, 1'b1}; // AND
+                        instruction_type_reg = AND; // AND
                         default: begin                    
                             instruction_format_reg = {5'bx};
                             instruction_type_reg = {23'bx};
                         end
                     endcase
-                    instruction_format_reg = {5'b10000};
+                    instruction_format_reg = R_FORMAT;
                 end
                 
                 default: begin
--- HW4/HW4.v
+++ HW4/HW4.v
@@ -11,14 +11,207 @@ module HW4(clk,
     input         clk, rst_n        ;
     output [31:2] mem_addr_I        ;
     input  [31:0] mem_rdata_I       ;
-	output [11:0] ctrl_signal  ;
-	output [31:0] immediate;
-    
+	output reg [11:0] ctrl_signal  ;
+	output reg [31:0] immediate;
+
+	// definition of operations
+	parameter ADD_OP = 4'b0;
+	parameter SLL_OP = 4'b0001;
+	parameter SLT_OP = 4'b0010;
+	parameter XOR_OP = 4'b0100;
+	parameter SRL_OP = 4'b0101;
+	parameter OR_OP  = 4'b0110;
+	parameter AND_OP = 4'b0111;
+	parameter SUB_OP = 4'b1000;
+	parameter SRA_OP = 4'b1101;
+
 	// wire/reg 
+	reg [11:0] ctrl_sig_next;
+	reg [31:0] immediate_next;
+
+	wire [22:0] instruction_type;
+	wire [ 4:0] instruction_format;
 	
+	HW3 decoder(
+        clk,
+		rst_n,
+		mem_addr_I,
+		mem_rdata_I,
+		instruction_type,
+		instruction_format,
+    );
+
+	/*
+	always @(*) begin
+		case (instruction_format)
+			decoder.R_FORMAT: begin
+				case (instruction_type)
+					decoder.ADD:
+						ctrl_sig_next[3:0] = ADD_OP;
+					decoder.SUB:
+						ctrl_sig_next[3:0] = SUB_OP;
+					decoder.SLL:
+						ctrl_sig_next[3:0] = SLL_OP;
+					decoder.SLT:
+						ctrl_sig_next[3:0] = SLT_OP;
+					decoder.XOR:
+						ctrl_sig_next[3:0] = XOR_OP;
+					decoder.SRL:
+						ctrl_sig_next[3:0] = SLL_OP;
+					decoder.SRA:
+						ctrl_sig_next[3:0] = SRA_OP;
+					decoder.OR:
+						ctrl_sig_next[3:0] = OR_OP;
+					decoder.AND:
+						ctrl_sig_next[3:0] = AND_OP;
+					default:
+						ctrl_sig_next[3:0] = 4'bx;
+				endcase
+			end
+
+			decoder.I_FORMAT: begin
+				immediate_next[11:0] = mem_rdata_I[31:20];
+				ctrl_sig_next[11:4] = 8'b00000011;
 	
+				case (instruction_type)
+					decoder.JAL:
+						ctrl_sig_next = 12'b10000x1xxxxx;
+					decoder.ADDI:
+						ctrl_sig_next[3:0] = ADD_OP;
+					decoder.SLLI:
+						ctrl_sig_next[3:0] = SLL_OP;
+					decoder.SLTI:
+						ctrl_sig_next[3:0] = SLT_OP;
+					decoder.XORI:
+						ctrl_sig_next[3:0] = XOR_OP;
+					decoder.SRLI:
+						ctrl_sig_next[3:0] = SLL_OP;
+					decoder.SRAI:
+						ctrl_sig_next[3:0] = SRA_OP;
+					decoder.ORI:
+						ctrl_sig_next[3:0] = OR_OP;
+					decoder.ANDI:
+						ctrl_sig_next[3:0] = AND_OP;
+					default:
+						ctrl_sig_next[3:0] = 4'bx;
+				endcase
+			end
+
+			decoder.S_FORMAT: begin
+				//                     11:5                4:0
+				immediate_next[11:0] = {mem_rdata_I[31:25], mem_rdata_I[11:7]};
+				ctrl_sig_next = 12'b00001x01xxxx;
+			end
+
+			decoder.B_FORMAT: begin
+				//                     12               11              10:5                4:1
+				immediate_next[12:1] = {mem_rdata_I[31], mem_rdata_I[7], mem_rdata_I[31:25], mem_rdata_I[11:7]};
+				ctrl_sig_next = 12'b00100x00xxxx;
+			end
+
+			decoder.J_FORMAT: begin // JALR
+				//                     20               19:12               11               10:1
+				immediate_next[20:1] = {mem_rdata_I[31], mem_rdata_I[19:12], mem_rdata_I[20], mem_rdata_I[30:21]}; // JAL imm[20|10:1|11|19:12]
+				ctrl_sig_next = 12'b10000x1xxxxx;
+			end
+
+
+			default: begin
+				ctrl_sig_next = 12'bx;
+				immediate_next = 32'bx;
+			end
+		endcase
+	end
+	*/
+	always @(posedge clk or negedge rst_n) begin
+		if (!rst_n) begin
+			ctrl_signal <= 12'b0;
+			immediate <= 32'b0;
+			// assign ctrl_signal = 12'b0;
+			// assign immediate = 32'b0;
+		end
+		else begin
+			// ctrl_signal <= ctrl_sig_next;
+		    // immediate <= immediate_next;
+			case (instruction_format)
+			decoder.R_FORMAT: begin
+				case (instruction_type)
+					decoder.ADD:
+						ctrl_signal[3:0] <= ADD_OP;
+					decoder.SUB:
+						ctrl_signal[3:0] <= SUB_OP;
+					decoder.SLL:
+						ctrl_signal[3:0] <= SLL_OP;
+					decoder.SLT:
+						ctrl_signal[3:0] <= SLT_OP;
+					decoder.XOR:
+						ctrl_signal[3:0] <= XOR_OP;
+					decoder.SRL:
+						ctrl_signal[3:0] <= SLL_OP;
+					decoder.SRA:
+						ctrl_signal[3:0] <= SRA_OP;
+					decoder.OR:
+						ctrl_signal[3:0] <= OR_OP;
+					decoder.AND:
+						ctrl_signal[3:0] <= AND_OP;
+					default:
+						ctrl_signal[3:0] <= 4'bx;
+				endcase
+			end
+
+			decoder.I_FORMAT: begin
+				immediate[11:0] <= mem_rdata_I[31:20];
+				ctrl_signal[11:4] <= 8'b00000011;
 	
+				case (instruction_type)
+					decoder.JAL:
+						ctrl_signal <= 12'b10000x1xxxxx;
+					decoder.ADDI:
+						ctrl_signal[3:0] <= ADD_OP;
+					decoder.SLLI:
+						ctrl_signal[3:0] <= SLL_OP;
+					decoder.SLTI:
+						ctrl_signal[3:0] <= SLT_OP;
+					decoder.XORI:
+						ctrl_signal[3:0] <= XOR_OP;
+					decoder.SRLI:
+						ctrl_signal[3:0] <= SLL_OP;
+					decoder.SRAI:
+						ctrl_signal[3:0] <= SRA_OP;
+					decoder.ORI:
+						ctrl_signal[3:0] <= OR_OP;
+					decoder.ANDI:
+						ctrl_signal[3:0] <= AND_OP;
+					default:
+						ctrl_signal[3:0] <= 4'bx;
+				endcase
+			end
+
+			decoder.S_FORMAT: begin
+				//                     11:5                4:0
+				immediate[11:0] <= {mem_rdata_I[31:25], mem_rdata_I[11:7]};
+				ctrl_signal <= 12'b00001x01xxxx;
+			end
+
+			decoder.B_FORMAT: begin
+				//                     12               11              10:5                4:1
+				immediate[12:1] <= {mem_rdata_I[31], mem_rdata_I[7], mem_rdata_I[31:25], mem_rdata_I[11:7]};
+				ctrl_signal <= 12'b00100x00xxxx;
+			end
+
+			decoder.J_FORMAT: begin // JALR
+				//                     20               19:12               11               10:1
+				immediate[20:1] <= {mem_rdata_I[31], mem_rdata_I[19:12], mem_rdata_I[20], mem_rdata_I[30:21]}; // JAL imm[20|10:1|11|19:12]
+				ctrl_signal <= 12'b10000x1xxxxx;
+			end
+
+			default: begin
+				ctrl_signal <= 12'bx;
+				immediate <= 32'bx;
+			end
+		endcase
+		end
+	end
+
 	// Connect to your HW3 module
-
-
 endmodule
