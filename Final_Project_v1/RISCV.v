dca1f4c1fbd5ccc7830a15242596083ddc5f6152
orange2120 <orange21201@gmail.com>
2020-12-30 01:49:47 +0800

    add migrate hw4 to final

--- Final_Project_v1/RISCV.v
+++ Final_Project_v1/RISCV.v
@@ -11,5 +11,36 @@ module RISCV(clk,
             mem_addr_I,
             mem_rdata_I);
 
+    input clk, rst_n;
+    output reg [31:2] mem_addr_I ;
+    input  [31:0] mem_rdata_I    ;
+    output mem_wen_D ;
+    output reg [31:2] mem_addr_D;
+    output reg [63:0] mem_wdata_D;
+    input [63:0] mem_rdata_D;
+
+    Controller ctrl(clk,
+            rst_n,
+            // mem_addr_I,
+            mem_rdata_I,
+			ctrl_signal,
+			immediate,
+			);
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+        end
+            mem_addr_I <= 30'b0;
+            mem_addr_D <= 30'b0;
+            mem_wen_D <= 1'b0;
+            mem_wdata_D <= 64'b0;
+
+        else begin
+            mem_addr_I <= mem_addr_I + 1'b1;
+
+        end
+    end
+
+
 
 endmodule
